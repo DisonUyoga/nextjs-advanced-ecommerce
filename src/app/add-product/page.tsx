1c15f64f7c0ece27609aede7cc445ed104bed93a
DisonUyoga <disonobudho233@gmail.com>
2023-12-24 07:47:06 -0800

    'auth'

--- src/app/add-product/page.tsx
+++ src/app/add-product/page.tsx
@@ -2,7 +2,7 @@ import { prisma } from '@/lib/db/prisma'
 import FormSubmitButton from '@/components/FormSubmitButton'
 import { redirect } from 'next/navigation'
 import { getServerSession } from 'next-auth'
-import { authOptions } from '../api/auth/[...nextauth]/route'
+import { authOptions } from '@/lib/authOptions'
 export const metadata = {
   title: 'Comfort | add product',
 }
